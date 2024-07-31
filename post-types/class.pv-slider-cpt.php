816ed179463c690f86ffd4f4db7b1de990d9d897
Paulo Fernandes Vieira <soeupfv@hotmail.com>
2024-07-31 03:45:54 -0300

    add metod save_post

--- post-types/class.pv-slider-cpt.php
+++ post-types/class.pv-slider-cpt.php
@@ -6,6 +6,7 @@ if(!class_exists( 'PV_Slider_Post_type' )){
     function __construct(){
       add_action( 'init', array( $this, 'create_post_type' ) );
       add_action( 'add_meta_boxes', array( $this, 'add_meta_boxes' ) );
+      add_action( 'save_post', array( $this, 'save_post' ), 10, 2 );
     }
 
     public function create_post_type(){
@@ -46,7 +47,18 @@ if(!class_exists( 'PV_Slider_Post_type' )){
       );
     }
     public function add_inner_meta_boxes( $post/*, $test */){
+      require_once( PV_SLIDER_PATH . 'views/pv-slider_metabox.php');
+    }
+    public function save_post($post_id){
+      if( isset($post_id['action']) && $post_id['action'] == 'editpost' ){
+        $old_link_text = get_post_meta( $post_id, 'pv_slider_link_text', true );
+        $new_link_text = $_POST['pv_slider_link_text'];
+        $old_link_url = get_post_meta( $post_id, 'pv_slider_link_text', true );
+        $new_link_url = $_POST['pv_slider_link_url'];;
 
+        update_post_meta($post_id, 'pv_slider_link_text', $new_link_text, $old_link_text);
+        update_post_meta($post_id, 'pv_slider_link_url', $new_link_url, $old_link_url);
+      }
     }
   }
 }
---
+++ views/pv-slider_metabox.php
@@ -0,0 +1,33 @@
+<table class="from-table pv-slider-metabox"> 
+  <tr>
+    <th>
+      <label for="pv_slider_link_text"> Link Text </label>
+    </th>
+    <td>
+      <input 
+      type="text" 
+      name="pv_slider_link_text"
+      id="pv_slider_link_text"
+      class="regular-text link-text"
+      value=""
+      required
+      >
+    </td>
+  </tr>
+
+  <tr>
+    <th>
+      <label for="pv_slider_link_url"> Link URL </label>
+    </th>
+    <td>
+      <input 
+      type="url" 
+      name="pv_slider_link_url"
+      id="pv_slider_link_url"
+      class="regular-text link-url"
+      value=""
+      required
+      >
+    </td>
+  </tr>
+</table>
