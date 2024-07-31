6e009bc824965a91c7e83470f2474592e58cd87a
Paulo Fernandes Vieira <soeupfv@hotmail.com>
2024-07-31 03:12:17 -0300

    add meta box

--- post-types/class.pv-slider-cpt.php
+++ post-types/class.pv-slider-cpt.php
@@ -4,7 +4,8 @@ if(!class_exists( 'PV_Slider_Post_type' )){
   class PV_Slider_Post_type{
 
     function __construct(){
-      add_action( 'init', array( $this, 'create_post_type') );
+      add_action( 'init', array( $this, 'create_post_type' ) );
+      add_action( 'add_meta_boxes', array( $this, 'add_meta_boxes' ) );
     }
 
     public function create_post_type(){
@@ -16,9 +17,35 @@ if(!class_exists( 'PV_Slider_Post_type' )){
             'singular_name' => __('Slider')
           ),
           'public' => true,
-          'has_archive' => true
+          'supports' => array('title', 'editor', 'thumbnail'),
+          'hierarchical' => false,
+          'show_ui' => true,
+          'show_in_menu' => true,
+          'menu_position' => 5,
+          'show_in_admin_bar' => true,
+          'show_in_nav_menus' => true,
+          'can_export' => true,
+          'has_archive' => true,
+          'exclude_from_search' => false,
+          'publicly_queryable' => true,
+          'show_in_rest' => true,
+          'menu_icon' => 'dashicons-images-alt2',
+          //'register_meta_box_cb' => array( $this, 'add_meta_boxes' )
         )
       );
+    }
+    public function add_meta_boxes(){
+      add_meta_box(
+        'pv_slider_meta_box',
+        'Link Options',
+        array( $this, 'add_inner_meta_boxes' ),
+        'pv-slider',
+        'normal'/*'side'*/,
+        'high'/*,
+        array( 'foo' => 'bar' )*/
+      );
+    }
+    public function add_inner_meta_boxes( $post/*, $test */){
 
     }
   }
