bad90687636e36134fe255af35b486986ae7f2a3
Paulo Fernandes Vieira <soeupfv@hotmail.com>
2024-08-02 00:12:04 -0300

    add verify atentication post and att metabox

--- post-types/class.pv-slider-cpt.php
+++ post-types/class.pv-slider-cpt.php
@@ -46,18 +46,47 @@ if(!class_exists( 'PV_Slider_Post_type' )){
         array( 'foo' => 'bar' )*/
       );
     }
+    
     public function add_inner_meta_boxes( $post/*, $test */){
       require_once( PV_SLIDER_PATH . 'views/pv-slider_metabox.php');
     }
+
     public function save_post($post_id){
+      if( isset( $_POST['pv_slider_nonce'] ) ){
+        if( ! wp_verify_nonce( $_POST['pv_slider_nonce'], 'pv_slider_nonce' ) ){
+          return;
+        }
+      }
+
+      if( defined( 'DOING_AUTOSAVE' ) && DOING_AUTOSAVE ){
+        return;
+      }
+
+      if( isset( $_POST['post_type'] ) && $_POST['post_type'] === 'pv_sldier' ){
+        if( ! current_user_can( 'edit_page', $post_id ) ){
+          return;
+        }elseif( ! current_user_can( 'edit_post', $post_id ) ){
+          return;
+        }
+      }
+
       if( isset($_POST['action']) && $_POST['action'] == 'editpost' ){
         $old_link_text = get_post_meta( $post_id, 'pv_slider_link_text', true );
         $new_link_text = $_POST['pv_slider_link_text'];
         $old_link_url = get_post_meta( $post_id, 'pv_slider_link_url', true );
-        $new_link_url = $_POST['pv_slider_link_url'];;
+        $new_link_url =  $_POST['pv_slider_link_url'];
 
-        update_post_meta($post_id, 'pv_slider_link_text', $new_link_text, $old_link_text);
-        update_post_meta($post_id, 'pv_slider_link_url', $new_link_url, $old_link_url);
+        if(empty( $new_link_text ) ){
+          update_post_meta($post_id, 'pv_slider_link_text', 'add some text');
+        }else{
+          update_post_meta($post_id, 'pv_slider_link_text', sanitize_text_field($new_link_text), $old_link_text);
+        }
+        if( empty( $new_link_url ) ){
+          update_post_meta($post_id, 'pv_slider_link_url', '#');
+        }else{
+          update_post_meta($post_id, 'pv_slider_link_url', esc_url_raw($new_link_url), $old_link_url);
+        }
+        
       }
     }
   }
--- views/pv-slider_metabox.php
+++ views/pv-slider_metabox.php
@@ -1,4 +1,11 @@
+<?php
+  $meta = get_post_meta( $post->ID );
+  $link_text = get_post_meta( $post->ID, 'pv_slider_link_text', true );
+  $link_url = get_post_meta( $post->ID, 'pv_slider_link_url', true );
+?>
+
 <table class="from-table pv-slider-metabox"> 
+<input type="hidden" name="pv_slider_nonce" value="<?php echo wp_create_nonce("pv_slider_nonce"); ?>">
   <tr>
     <th>
       <label for="pv_slider_link_text"> Link Text </label>
@@ -9,7 +16,7 @@
       name="pv_slider_link_text"
       id="pv_slider_link_text"
       class="regular-text link-text"
-      value=""
+      value="<?php echo ( isset( $link_text ) ) ? esc_html($link_text) : ''; ?>"
       required
       >
     </td>
@@ -25,7 +32,7 @@
       name="pv_slider_link_url"
       id="pv_slider_link_url"
       class="regular-text link-url"
-      value=""
+      value="<?php echo ( isset( $link_url ) ) ? esc_url($link_url/*$meta['pv_slider_link_url'][0]*/) : ''; ?>"
       required
       >
     </td>
